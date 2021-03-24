0e79c410c632abc04094d3705181b2cc8b4eb3c4
abdouudrh <abdelrrahman3@gmail.com>
2021-03-24 01:29:44 +0100

    my first commit

---
+++ src/components/userList.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const UserList = (props) => {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("https://jsonplaceholder.typicode.com/users");
+
+      setUser(result.data);
+      console.log(typeof result.data);
+      console.log(result.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <ul className="list" >
+      {user.map((item) => (
+        <li className="userslist"   key={item.id}>
+          <h2 className="name" >name : {item.name} </h2>
+          <h4 className="username" >username : {item.username}</h4>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default UserList;
+
+
+
+
+
